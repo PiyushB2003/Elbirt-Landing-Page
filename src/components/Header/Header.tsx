959caa4da981c02db78145da64dca913c1a3d4b2
piyush-radical <piyush.b@radicalglobal.net>
2024-12-10 00:32:21 +0530

    change in header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,14 +4,16 @@ const Header = () => {
     return (
         <div className="w-full h-[50px] flex items-center">
             <div className="absolute left-[200px]">
-                <Image 
+                {/* <Image 
                     src="/images/elbrit-logo.png" 
                     alt="elbrit-logo" 
                     width={200} 
                     height={61}
                     placeholder="blur"
                     blurDataURL="data:image/jpeg;base64,..."
-                />
+                /> */}
+
+                <img src="/images/elbrit-logo.png" alt="elbrit-kaun" />
             </div>
         </div>
     )
